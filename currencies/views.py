570790a8f3b5b0247ceb62448be53afb011fec79
Wil Tan <wil@dready.org>
2015-01-12 16:24:18 +1100

    set_currency: check next url is safe, simplify code

--- currencies/views.py
+++ currencies/views.py
@@ -1,18 +1,16 @@
 from django.http import HttpResponseRedirect
+from django.utils.http import is_safe_url
 from currencies.models import Currency
 
 
 def set_currency(request):
-    if request.method == 'POST':
-        currency_code = request.POST.get('currency', None)
-        next = request.POST.get('next', None)
+    currency_code = request.POST.get('currency', request.GET.get('currency'))
+    next = request.POST.get('next', request.GET.get('next'))
+    if next:
+        if not is_safe_url(url=next, host=request.get_host()):
+            next = '/'
     else:
-        currency_code = request.GET.get('currency', None)
-        next = request.GET.get('next', None)
-    if not next:
-        next = request.META.get('HTTP_REFERER', None)
-    if not next:
-        next = '/'
+        next = request.META.get('HTTP_REFERER', None) or '/'
 
     response = HttpResponseRedirect(next)
     if currency_code:
